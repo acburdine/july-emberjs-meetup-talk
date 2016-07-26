719c6a42511df179a573d73c179686e6caceeae3
Austin Burdine <acburdine@gmail.com>
2016-07-26 17:58:03 -0600

    add basic slide framework

---
+++ app/controllers/application.js
@@ -0,0 +1,4 @@
+import Controller from 'ember-controller';
+import SlideShowController from "ember-slide-show/mixins/slide-show-controller";
+
+export default Controller.extend(SlideShowController);
---
+++ app/routes/application.js
@@ -0,0 +1,10 @@
+import Route from 'ember-route';
+
+export default Route.extend({
+    model() {
+        return [
+            'intro',
+            'whoami'
+        ];
+    }
+});
