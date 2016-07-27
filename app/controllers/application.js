2ecf954be08deeb86c8d6f2514fc6bca400c455f
Austin Burdine <acburdine@gmail.com>
2016-07-26 18:13:43 -0600

    Add keyboard handling to change slides

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,20 @@
 import Controller from 'ember-controller';
+import on from 'ember-evented/on';
 import SlideShowController from "ember-slide-show/mixins/slide-show-controller";
+import {EKMixin, keyPress} from 'ember-keyboard';
 
-export default Controller.extend(SlideShowController);
+export default Controller.extend(SlideShowController, EKMixin, {
+    init() {
+        this._super(...arguments);
+
+        this.set('keyboardActivated', true);
+    },
+
+    previousSlide: on(keyPress('ArrowLeft'), function () {
+        this.decrementProperty('slideIndex');
+    }),
+
+    nextSlide: on(keyPress('ArrowRight'), function () {
+        this.incrementProperty('slideIndex');
+    })
+});
